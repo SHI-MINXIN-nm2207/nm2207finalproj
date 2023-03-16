f2138ffd674ad519e850872ee76dccd28bd64842
SHI-MINXIN-nm2207 <125366023+SHI-MINXIN-nm2207@users.noreply.github.com>
2023-03-17 04:59:05 +0800

    Add files via upload

--- finalproject/js/ppt.js
+++ finalproject/js/ppt.js
@@ -23,113 +23,127 @@ const ctx = canvas.getContext('2d');
 //   { title: '', content: 'It doesn\'t matter. What matters is that \nyou\'re going to die of hunger.' }
 // ];
 
-const { width, height } = canvas
-let colors = []
+const { width, height } = canvas; // Get canvas width and height
+let particles = []; // Initialize particles array
 
+// Initialize background data with random x and y values
 const bgData = Array.from(new Array(100)).map(v => {
   return {
     x: Math.random() * width,
     y: Math.random() * height,
     step: Math.random() * 2.5 + 0.5
   }
-})
+});
 
+// Function to send text and create granulated text effect
 const sendText = (text, fontSize = ((width * 1.1) / text.length), stepV = 40) => {
-  ctx.font = `bold ${fontSize}px Montserrat`
-  ctx.fillStyle = '#000000'
-  ctx.fillRect(0, 0, width, height)
-  ctx.fillStyle = '#ffffff'
-  ctx.textAlign = 'center'
-  ctx.textBaseline = 'middle'
-  const lines = text.split('\n')
-  const lineCount = lines.length
-  let linesHeight = lineCount * fontSize / 2
+  ctx.font = `bold ${fontSize}px Montserrat`; // Set font style and size
+  ctx.fillStyle = '#000000'; // Set fill particle to black
+  ctx.fillRect(0, 0, width, height); // Fill the canvas with black particle
+  ctx.fillStyle = '#ffffff'; // Set fill particle to white
+  ctx.textAlign = 'center'; // Set text alignment to center
+  ctx.textBaseline = 'middle'; // Set text baseline to middle
+  const lines = text.split('\n'); // Split the text into lines
+  const lineCount = lines.length; // Get the number of lines
+  let linesHeight = lineCount * fontSize / 2; // Calculate lines height
   for (let i = 0; i < lineCount; i++) {
-    ctx.fillText(lines[i], width / 2, height / 2 - fontSize * (lineCount - 1 - i) + linesHeight - fontSize / 2)
-  }
-  const data = ctx.getImageData(0, 0, width, height).data
+    ctx.fillText(lines[i], width / 2, height / 2 - fontSize * (lineCount - 1 - i) + linesHeight - fontSize / 2);
+  } // Draw text lines
+  const data = ctx.getImageData(0, 0, width, height).data; // Get image data
 
-  let index = 0
-  let bl = 4
-  let useIndex = 0
+  let index = 0; // Initialize index variable
+  let bl = 4; // Set block size for granulated text effect
+  let useIndex = 0; // Initialize useIndex variable
   for (let i = 0; i < data.length; i += 4) {
-    const x = index % width
-    const y = Math.ceil(index / width)
+    const x = index % width; // Calculate x position
+    const y = Math.ceil(index / width); // Calculate y position
     if (x % bl === 0 && y % bl === 0 && data[i] === 255 && data[i + 1] === 255 && data[i + 2] === 255) {
-      const rx = Math.floor(Math.random() * fontSize) + width / 2 - fontSize / 2
-      const ry = Math.floor(Math.random() * fontSize) + height / 2 - fontSize / 2
-      const item = colors[useIndex]
-      if (item) {
-        colors[useIndex] = {
+      const rx = Math.floor(Math.random() * fontSize) + width / 2 - fontSize / 2; 
+      // Calculate random x position within the text bounds
+      const ry = Math.floor(Math.random() * fontSize) + height / 2 - fontSize / 2; 
+      // Calculate random y position within the text bounds
+      const item = particles[useIndex]; // Get the current particle item
+      if (item) { // If the particle item exists
+        particles[useIndex] = {
           x,
           y,
           rx: item.x,
           ry: item.y,
           stepX: Math.abs(item.x - x) / stepV,
           stepY: Math.abs(item.y - y) / stepV
-        }
-      } else {
-        colors[useIndex] = {
+        }; // Update the particle item with new position and step values
+      } else { // If the particle item doesn't exist
+        particles[useIndex] = {
           x,
           y,
           rx,
           ry,
           stepX: Math.abs(rx - x) / stepV,
           stepY: Math.abs(ry - y) / stepV
-        }
+        }; // Create a new particle item with position and step values
       }
-      useIndex++
+      useIndex++; // Increment useIndex
     }
-    index++
-  }
-  if (useIndex < colors.length) {
-    colors.splice(useIndex, colors.length - useIndex)
-  }
+    index++; // Increment index
+  } // Update particle positions for granulated text effect
+  if (useIndex < particles.length) {
+    particles.splice(useIndex, particles.length - useIndex);
+  } // Remove unnecessary elements from particles array
 }
 
+
+// Function to render the granulated text effect
 // make the text move, like granulation
 const render = () => {
-  ctx.beginPath()
-  ctx.clearRect(0, 0, width, height)
-  colors.forEach(v => {
-    if (v.rx > v.x) {
-      v.rx -= v.stepX
-      if (v.rx < v.x) {
-        v.rx = v.x
+  ctx.beginPath(); // Begin a new path for drawing
+  ctx.clearRect(0, 0, width, height); // Clear the canvas
+
+  particles.forEach(v => { // Iterate through each particle
+    if (v.rx > v.x) { // If the current x position is greater than the target x position
+      v.rx -= v.stepX; // Move the particle left
+      if (v.rx < v.x) { // If the new position goes past the target
+        v.rx = v.x; // Set the position to the target
       }
-    } else if (v.rx < v.x) {
-      v.rx += v.stepX
-      if (v.rx > v.x) {
-        v.rx = v.x
-      }
-    }
-    if (v.ry > v.y) {
-      v.ry -= v.stepY
-      if (v.ry < v.y) {
-        v.ry = v.y
-      }
-    } else if (v.ry < v.y) {
-      v.ry += v.stepY
-      if (v.ry > v.y) {
-        v.ry = v.y
+    } else if (v.rx < v.x) { // If the current x position is less than the target x position
+      v.rx += v.stepX; // Move the particle right
+      if (v.rx > v.x) { // If the new position goes past the target
+        v.rx = v.x; // Set the position to the target
       }
     }
-    ctx.rect(v.rx, v.ry, 3, 3)
-  })
-  bgData.forEach(v => {
-    v.y = v.y > height ? 0 : (v.y + v.step)
-    ctx.rect(v.x, v.y, 2, 2)
-  })
-  ctx.fill()
-  requestAnimationFrame(render)
-}
+
+    if (v.ry > v.y) { // If the current y position is greater than the target y position
+      v.ry -= v.stepY; // Move the particle up
+      if (v.ry < v.y) { // If the new position goes past the target
+        v.ry = v.y; // Set the position to the target
+      }
+    } else if (v.ry < v.y) { // If the current y position is less than the target y position
+      v.ry += v.stepY; // Move the particle down
+      if (v.ry > v.y) { // If the new position goes past the target
+        v.ry = v.y; // Set the position to the target
+      }
+    }
+
+    ctx.rect(v.rx, v.ry, 3, 3); // Draw a rectangle for the particle
+  });
+
+  bgData.forEach(v => { // Iterate through each background data item
+    v.y = v.y > height ? 0 : (v.y + v.step); // Move the item down, and reset to 0 if it goes past the height
+    ctx.rect(v.x, v.y, 2, 2); // Draw a rectangle for the background data item
+  });
+
+  ctx.fill(); // Fill the drawn rectangles
+  requestAnimationFrame(render); // Request the next frame to continue animation
+};
+
 
 render()
 
+// Function to send text and create granulated text effect
 const awaitSendText = async (txt, fontSize, stepV) => {
   return new Promise((resolve) => {
     sendText(txt, fontSize, stepV)
-    colors.sort(v => Math.random() - 0.5)
+    particles.sort(v => Math.random() - 0.5)
+    // setting the timeout to 4160ms
     setTimeout(() => resolve(), 4160 + (stepV > 40 ? 1000 : 0))
   })
 }
--- finalproject/js/snake.js
+++ finalproject/js/snake.js
@@ -1,3 +1,5 @@
+// reference: https://segmentfault.com/a/1190000041933671
+
 // the velocity of the snake
 var SnakeTime = 200;
 // map
@@ -107,6 +109,7 @@ function Snake() {
         }
 
         // 根据方向处理蛇头
+        // change the head of the snake according to the direction
         switch (this.direction) {
             case "left":
                 this.body[0].x -= 1;
@@ -145,11 +148,17 @@ function Snake() {
         // 判断是否出界,根据蛇头判断
         // judge whether the snake is out of the map, according to the head of the snake
         if (this.body[0].x < 0 || this.body[0].x > 120 || this.body[0].y < 0 || this.body[0].y > 60) {
-            clearInterval(timer);
             // 清除定时器
             // clear the timer
+            clearInterval(timer);
+            // 弹出提示框
+            // pop up the message
             popMsg('You out of the map! You died')
+            // 显示重新开始按钮
+            // display the button of restart
             document.getElementById('beginBox').style.display = 'block';
+            // 删除旧的蛇
+            // delete the old snake
             for (var i = 0; i < this.body.length; i++) {
                 if (this.body[i].flag != null) {
 
@@ -190,6 +199,7 @@ function Snake() {
             // clear the food, and generate a new food
             map.removeChild(food.flag);
             food.display();
+            startTime = new Date().getTime();  // reset the start time
         }
 
         // 判断蛇头吃到毒药，xy坐标重合，
@@ -211,16 +221,28 @@ function Snake() {
                 SnakeTime = 210;
             }
             SnakeTime = SnakeTime + (len - 3) * 5;
-            // SnakeTime最低不能小于40
-            // SnakeTime can't be less than 40
+            // SnakeTime最长不能大于200
+            // SnakeTime can't be more than 200
             if (SnakeTime > 200) {
                 SnakeTime = 200;
             }
+            // 当蛇长度小于3时，游戏结束
+            // stop the game when the length of the snake is less than 3
             if (this.body.length < 3) {
+                // 蛇太短，游戏结束
+                // the snake is too short, the game is over
                 popMsg('You are too short to live!')
+                // 清除定时器
+                // clear the timer
                 clearInterval(timer);
+                // 设置游戏状态为结束
+                // set the game status to end
                 flagBagin = false;
+                // 显示开始按钮
+                // display the start button
                 document.getElementById('beginBox').style.display = 'block';
+                // 清除蛇
+                // clear the snake
                 for (var i = 0; i < this.body.length; i++) {
                     if (this.body[i].flag != null) {
                         map.removeChild(this.body[i].flag);
@@ -232,14 +254,15 @@ function Snake() {
                     { x: 0, y: 0 }
                 ];
                 this.direction = 'down';
-                this.display();   // 显示初始状态
-                return false;   // 结束
+                this.display();   // 显示初始状态  // display the initial status
+                return false;   // 结束 // end
             }
             refresh();
             // 清除毒药,重新生成毒药
             // clear the wrong food, and generate a new wrong food
             map.removeChild(wrongfood.flag);
             wrongfood.display();
+            startTime = new Date().getTime();  // reset the start time
 
         }
         if (this.body[0].x == correctfood.x && this.body[0].y == correctfood.y) {
@@ -262,6 +285,7 @@ function Snake() {
             // 清除食物,重新生成食物
             map.removeChild(correctfood.flag);
             correctfood.display();
+            startTime = new Date().getTime();  // reset the start time
         }
 
         // 吃到自己死亡，从第五个开始与头判断，因为前四个永远撞不到
@@ -269,9 +293,17 @@ function Snake() {
         for (var i = 4; i < this.body.length; i++) {
             if (this.body[0].x == this.body[i].x && this.body[0].y == this.body[i].y) {
                 clearInterval(timer);   // clear the timer
+                // 弹出提示框
+                // pop up the message box
                 popMsg('You eat yourself! You are dead!')
+                //设置游戏状态为结束
+                // set the game status to end
                 flagBagin = false;
+                // 显示开始按钮
+                // show the begin button
                 document.getElementById('beginBox').style.display = 'block';
+                // 删除蛇
+                // delete the snake
                 for (var i = 0; i < this.body.length; i++) {
                     if (this.body[i].flag != null) {
                         map.removeChild(this.body[i].flag);
@@ -283,8 +315,8 @@ function Snake() {
                     { x: 0, y: 0 }
                 ];
                 this.direction = 'down';
-                this.display();   // 显示初始状态
-                return false;   // 结束
+                this.display();   // 显示初始状态 show the initial status
+                return false;   // 结束 end
             }
         }
         if (this.body.length > 15) {
@@ -309,95 +341,171 @@ function Snake() {
 
 // 构造食物
 function Food() {
+    // 食物的宽和高
+    // width and height of the food
     this.width = 10;
     this.height = 10;
 
+    // 食物的显示方法
+    // display method of the food
     this.display = function () {
-        // 创建一个div(一节蛇身)
+        // 创建一个 div 作为食物
+        // create a div as the food
         var f = document.createElement('div');
+        // 为当前食物添加标记
+        // add a flag to the current food
         this.flag = f;
+        // 设置食物的样式
+        // set the style of the food
         f.style.width = this.width + 'px';
         f.style.height = this.height + 'px';
         f.style.background = 'red';
         f.style.position = 'absolute';
+        // 随机生成食物的位置
+        // generate the position of the food randomly
         this.x = Math.floor(Math.random() * 100) + 10;
         this.y = Math.floor(Math.random() * 50) + 5;
+        // 如果食物位置和错误食物位置重合，则修改食物的横纵坐标
+        // if the position of the food is the same as the wrong food, then change the position of the food
         if (this.x == wrongfood.x && this.y == wrongfood.y) {
             this.x = this.x + 1;
             this.y = this.y + 1;
+        // 如果食物位置和药物食物位置重合，则修改食物的横纵坐标
+        // if the position of the food is the same as the correct food, then change the position of the food
         } else if (this.x == correctfood.x && this.y == correctfood.y) {
             this.x = this.x + 1;
             this.y = this.y + 1;
         }
+        // 设置食物的位置
+        // set the position of the food
         f.style.left = this.x * this.width + 'px';
         f.style.top = this.y * this.height + 'px';
+        // 将食物添加到地图中
+        // add the food to the map
         map.appendChild(f);
     }
 }
 
+
 function wrongfood() {
+    // 毒药宽度
+    // wrong food width
     this.width = 10;
+    // 毒药高度
+    // wrong food height
     this.height = 10;
+    // 显示毒药
+    // display the wrong food
     this.display = function () {
+        // 创建 div 元素
+        // create a div element
         var f = document.createElement('div');
+        // 将创建的 div 元素赋值给 flag 属性
+        // assign the created div element to the flag property
         this.flag = f;
+        // 设置 div 元素的宽度
+        // set the width of the div element
         f.style.width = this.width + 'px';
+        // 设置 div 元素的高度
+        // set the height of the div element
         f.style.height = this.height + 'px';
+        // 设置 div 元素的背景颜色为绿色
+        // set the background color of the div element to green
         f.style.background = 'green';
+        // 设置 div 元素的定位方式为绝对定位
+        // set the positioning method of the div element to absolute positioning
         f.style.position = 'absolute';
+        // 随机生成食物的横坐标和纵坐标
+        // randomly generate the horizontal coordinate and vertical coordinate of the food
         this.x = Math.floor(Math.random() * 100) + 10;
         this.y = Math.floor(Math.random() * 50) + 5;
+        // 如果毒药的横纵坐标与正确的食物的横纵坐标相同，则修改毒药的横纵坐标
+        // if the horizontal and vertical coordinates of the wrong food are the same as the horizontal and vertical coordinates of the correct food, modify the horizontal and vertical coordinates of the wrong food
         if (this.x == food.x && this.y == food.y) {
             this.x = this.x + 1;
             this.y = this.y + 1;
+        // 如果毒药的横纵坐标与药物的横纵坐标相同，则修改毒药的横纵坐标
+        // if the horizontal and vertical coordinates of the wrong food are the same as the horizontal and vertical coordinates of the drug, modify the horizontal and vertical coordinates of the wrong food
         } else if (this.x == correctfood.x && this.y == correctfood.y) {
             this.x = this.x + 2;
             this.y = this.y + 2;
         }
+        // 设置毒药元素的左边距和上边距
+        // set the left margin and top margin of the wrong food element
         f.style.left = this.x * this.width + 'px';
         f.style.top = this.y * this.height + 'px';
+        // 将毒药添加到地图上
+        // add the wrong food to the map
         map.appendChild(f);
     }
 }
 
+
+
 function correctfood() {
+    // 定义 correctfood 构造函数，初始化蛇吃到药物食物时的样式和位置信息
+    // define the correctfood constructor, initialize the style and position information of the snake when it eats the drug food
     this.width = 10;
+    // 药物食物的高度
+    // the height of the drug food
     this.height = 10;
+    // 显示药物食物
+    // display the drug food
     this.display = function () {
+        // 创建 div 元素作为药物食物
+        // create a div element as a drug food
         var f = document.createElement('div');
+        // 为 correctfood 对象绑定 DOM 元素
+        // bind the DOM element to the correctfood object
         this.flag = f;
+        // 设置药物食物的宽度
+        // set the width of the drug food
         f.style.width = this.width + 'px';
         f.style.height = this.height + 'px';
+        // set the background color of the drug food
         f.style.background = 'blue';
         f.style.position = 'absolute';
+        // 生成随机的坐标
+        // generate random coordinates
         this.x = Math.floor(Math.random() * 100) + 10;
         this.y = Math.floor(Math.random() * 50) + 5;
+        // 判断药物食物和错误食物，普通食物的位置是否重合，如果重合，则调整药物食物的位置
+        // judge whether the position of the drug food and the wrong food, the normal food overlaps, if it overlaps, adjust the position of the drug food
         if (this.x == food.x && this.y == food.y) {
-            this.x = this.x + 1;
-            this.y = this.y + 1;
+        this.x = this.x + 1;
+        this.y = this.y + 1;
         } else if (this.x == wrongfood.x && this.y == wrongfood.y) {
-            this.x = this.x + 1;
-            this.y = this.y + 1;
+        this.x = this.x + 1;
+        this.y = this.y + 1;
         }
+        // 设置药物食物的位置
+        // set the position of the drug food
         f.style.left = this.x * this.width + 'px';
         f.style.top = this.y * this.height + 'px';
+        // 将药物食物添加到地图上
+        // add the drug food to the map
         map.appendChild(f);
     }
-}
+  }
+  
 
 
 // 给body加按键事件，上下左右
+// add keydown event to body, up, down, left, right
 document.body.onkeydown = function (e) {
     // 有事件对象就用事件对象，没有就自己创建一个，兼容低版本浏览器
+    // if there is an event object, use it, otherwise create one, compatible with low version browser
     var ev = e || window.event;
 
     switch (ev.keyCode) {
         case 38:
+            // not allow to return, when the snake is going up, it can't go down
             if (snake.direction != 'down') {   // 不允许返回，向上的时候不能向下
                 snake.direction = "up";
             }
             break;
         case 40:
+            // not allow to return, when the snake is going down, it can't go up
             if (snake.direction != "up") {
                 snake.direction = "down";
             }
@@ -413,6 +521,7 @@ document.body.onkeydown = function (e) {
             }
             break;
         // 兼容WASD键    
+        // compatible with WASD key
         case 87:
             if (snake.direction != "down") {
                 snake.direction = "up";
@@ -437,31 +546,41 @@ document.body.onkeydown = function (e) {
 
 };
 
+// 初始化显示
+// init display
 var snake = new Snake();
 var food = new Food();
-// 初始化显示
 snake.display();
 food.display();
+// 新定义的两种不同效果的食物
+// new define two different effect food
 var wrongfood = new wrongfood();
 wrongfood.display();
 var correctfood = new correctfood();
 correctfood.display();
 
+
 // 获取开始按钮
+// get begin button
 var btn = document.getElementById('begin');
 // 点击开始游戏事件
+// click begin button
 btn.onclick = function () {
     // 开始按钮毛玻璃幕布
-
+    // begin button curtain
     var parent = this.parentNode;
     // 隐藏开始按钮
+    // hide begin button
     parent.style.display = 'none';
     // 获取定时器时间
+    // get timer time
     SnakeTime = 200;
     let time2 = SnakeTime;
     flagBagin = true;
     startTime = Date.now();
     timeoutflag = false;
+    // 设置蛇动
+    // set snake move
     timer = setInterval(function () {
         snake.run();
     }, time2);
@@ -471,19 +590,25 @@ btn.onclick = function () {
 
 
 // 定义刷新定时器方法
+// define refresh timer method
 function refresh() {
     // 停止定时器
+    // stop timer
     clearInterval(timer);
     // 刷新定时器
+    // reset timer
     timer = setInterval(function () {
         snake.run();
     }, SnakeTime);
 }
 
 // 弹窗提示
+// pop up window
 function popMsg(msg, flag) {
     let dom = document.createElement('div')
     dom.textContent = msg
+    // 设置淡出效果
+    // set fade out effect
     dom.className = 'message'
     document.body.appendChild(dom)
     setTimeout(() => {
@@ -494,13 +619,18 @@ function popMsg(msg, flag) {
 }
 
 // 显示烟花/炸弹
+// show fireworks/bomb
 function showAni(flag = false) {
     let dom
+    // 根据是否获胜显示不同动画
+    // show different animation according to whether win
     if (flag) {
         dom = document.querySelector('.fire')
     } else {
         dom = document.querySelector('.bomb')
     }
+    // 设置淡入效果
+    // set fade in effect
     dom.style.display = 'block'
     setTimeout(() => {
         dom.style.display = 'none'
